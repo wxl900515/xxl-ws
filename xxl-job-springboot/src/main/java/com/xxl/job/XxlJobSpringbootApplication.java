364104ec108eb359f5d08498b22276a9c75a0789
wxl <164603821@qq.com>
2020-09-30 15:50:53 +0800

    11

---
+++ xxl-job-springboot/src/main/java/com/xxl/job/XxlJobSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.xxl.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XxlJobSpringbootApplication.class, args);
+	}
+
+}
---
+++ xxl-job-springboot/src/test/java/com/xxl/job/XxlJobSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxl.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxlJobSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
